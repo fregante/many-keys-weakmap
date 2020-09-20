2eb01222c6b15186954d0604272357f34a60439e
Federico Brigante <opensource@bfred.it>
2020-09-20 18:15:53 -0500

    Meta

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 declare const baseMap: unique symbol;
-interface NestedAnyMap<T extends Object> extends WeakMap<T, NestedAnyMap<T>>, Map<T, NestedAnyMap<T>> {
+interface NestedAnyMap<T extends Object, V> extends WeakMap<T, NestedAnyMap<T, V>>, Map<T, NestedAnyMap<T, V>> {
     [index: string]: any;
 }
-declare class ManyKeysWeakMap<T extends object> extends WeakMap {
-    [baseMap]: NestedAnyMap<T>;
+declare class ManyKeysWeakMap<K extends object[], V> extends WeakMap {
+    [baseMap]: NestedAnyMap<K, V>;
     constructor();
-    set<V, K>(keys: K[], value: V): this;
-    get<V, K>(keys: K[]): V;
+    set<K, V>(keys: K[], value: V): this;
+    get<K>(keys: K[]): V | undefined;
     has<K>(keys: K[]): boolean;
     delete<K>(keys: K[]): boolean;
     readonly [Symbol.toStringTag]: string;
