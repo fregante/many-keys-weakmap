9aca0ad3d1d8443d1e6499d7916dbb8de0d133d1
Federico Brigante <me@fregante.com>
2023-02-20 19:02:32 +0800

    Lint

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
+// eslint-disable-next-line @typescript-eslint/ban-types -- It matches WeakMap’s
 export class ManyKeysWeakMap<K extends object, V> extends WeakMap<K[], V> {}
